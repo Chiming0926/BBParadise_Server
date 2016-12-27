90b8a424465cdb751fee11156a315041dd6e2bf4
Chiming <Chiming0926@gmail.com>
2016-12-27 23:13:18 +0800

    first commit

---
+++ BBParadise_Server/BBParadise.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BBParadise_Server
+{
+    partial class BBParadise
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lable_status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lable_status
+            // 
+            this.lable_status.AutoSize = true;
+            this.lable_status.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lable_status.Location = new System.Drawing.Point(64, 49);
+            this.lable_status.Name = "lable_status";
+            this.lable_status.Size = new System.Drawing.Size(94, 19);
+            this.lable_status.TabIndex = 0;
+            this.lable_status.Text = "lable_status";
+            // 
+            // BBParadise
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 499);
+            this.Controls.Add(this.lable_status);
+            this.Name = "BBParadise";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lable_status;
+    }
+}
+
---
+++ BBParadise_Server/BBParadise.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BBParadise_Server
+{
+    public partial class BBParadise : Form
+    {
+        string gguid = "7245577f-4961-7642-a64c-ba5bb008892c";
+        string sguid = "52a06444-ff13-654b-bfa1-29da9f7124dd";
+
+        string iguid_player = "";
+        string sguid_game = "";
+        string lguid = "";
+
+        byte[] certificate = {0x43, 0x13, 0xbd, 0x25, 0x4e, 0x7b, 0xa3, 0x4b, 0x86, 0x13, 0xa8, 0xa5, 0x49,
+                            0xcf, 0xd1, 0x5e, 0x58, 0x34, 0x2c, 0xda, 0xb, 0x9f, 0xc, 0x41, 0x8a, 0x3e,
+                            0xd4, 0x71, 0x5a, 0xb, 0x3d, 0x41, 0x57, 0x1c, 0x53, 0xed, 0xf, 0x52, 0x70,
+                            0x47, 0x93, 0xb5, 0x9f, 0x27, 0xe3, 0xa0, 0x66, 0x2d, 0x86, 0x55, 0x5b, 0x9c,
+                            0x4b, 0x33, 0xd8, 0x40, 0xbb, 0x66, 0xf1, 0x8c, 0x67, 0x19, 0x8a, 0x4, 0x1a,
+                            0x14, 0x28, 0xa7, 0x67, 0x95, 0x72, 0x4c, 0x8c, 0xfb, 0xa, 0xf5, 0x4a, 0x12,
+                            0x49, 0x53, 0x6d, 0xf7, 0xa0, 0xd4, 0x73, 0x96, 0x52, 0x43, 0x8e, 0x54, 0x79,
+                            0xfa, 0x48, 0xeb, 0x5b, 0xaf, 0xd1, 0x64, 0x20, 0x3d, 0x49, 0xa5, 0xbc, 0x40,
+                            0x89, 0xa2, 0xb6, 0xc5, 0x6f, 0xd6, 0xac, 0xfe, 0x2f, 0x92, 0x4d, 0xbc, 0x3f,
+                            0xbd, 0x4b, 0x4d, 0x90, 0xf8, 0x50, 0xf2, 0x2, 0x16, 0x75, 0xd4};
+
+        public ArcaletGame ag = null;
+
+        string m_username = "su000001132119";
+        string m_password = "vhGujAMr";
+
+        public BBParadise()
+        {
+            InitializeComponent();
+            login();
+        }
+
+        void login()
+        {
+            lable_status.Text = "Prepare Login";
+            ag = new ArcaletGame(m_username, m_password, gguid, sguid, certificate);
+            ag.onMessageIn += MainMessageIn;
+            ag.onPrivateMessageIn += PrivateMessageIn;
+            ag.onStateChanged += OnStateChanged;
+            ag.onCompletion += CB_Login;
+            ag.Launch();
+        }
+
+        void MainMessageIn(string msg, int delay, ArcaletGame game)
+        {
+        /*    try
+            {
+                Console.WriteLine("MainMessageIn: " + msg);
+                string[] cmds = msg.Split(':');
+                switch (cmds[0])
+                {
+                    case "new": PlayerNew(cmds[1].Split('/')); break;
+                    case "quit": PlayerQuit(cmds[1]); break;
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e.ToString()); }
+            */
+        }
+
+        void PrivateMessageIn(string msg, int delay, ArcaletGame game)
+        {
+            try
+            {
+                Console.WriteLine("PrivateMessageIn: " + msg);
+                string[] cmds = msg.Split(':');
+                switch (cmds[0])
+                {
+                //    case "match": DP_Match(cmds[1]); break;
+                //    case "cancel": CancelMatchCheck(int.Parse(cmds[1])); break;
+                }
+            }
+            catch (Exception) { }
+        }
+
+        void GameMessageIn(string msg, int delay, ArcaletScene scene)
+        {
+            /*
+            try
+            {
+                Console.WriteLine("GameMessageIn: " + msg);
+                GameRoom room = scene.token as GameRoom;
+                string[] cmds = msg.Split(':');
+
+                if (cmds[0] == "dp_draw" || cmds[0] == "dp_gameover" || cmds[0] == "dp_timeup")
+                {
+                    room.GameOver();
+                    room.LeaveScene();
+                }
+
+                if (room.gameover)
+                    return;
+
+                switch (cmds[0])
+                {
+                    case "ready": PlayerReady(cmds[1], room); break;
+                    case "slot": SlotCheck(cmds[1], room); break;
+                }
+            }
+            catch (Exception) { }*/
+        }
+
+        void OnStateChanged(int state, int code, ArcaletGame game)
+        {
+            if (state >= 900)
+                lable_status.Text = "Disconnection: " + code;
+        }
+
+        void CB_Login(int code, ArcaletGame game)
+        {
+            if (code == 0)
+                lable_status.Text = "Login Successed";
+            else
+                lable_status.Text = "Login Failed " + code;
+        }
+    }
+}
